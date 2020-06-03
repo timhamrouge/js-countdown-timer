d97c27e82fe47600575dd43df638cbcf75b5b1d9
timhamrouge <t.hamrouge@gmail.com>
2020-06-03 15:40:14 +0100

    it opens a modal, stops the interval and plays a chicken noise for an alarm

--- countdown-timer.js
+++ countdown-timer.js
@@ -7,11 +7,8 @@
     //     dates = 
     // }
 
-        function updateDOMElementInnerHTML(elementID, val) {
+        function updateErrorStyling(elementID, val, colour) {
             document.getElementById(`event-${elementID}-error`).innerHTML = val;
-        }
-
-        function updateDOMElementColour(elementID, colour) {
             document.getElementById(`event-${elementID}`).style.border = colour;
         }
 
@@ -19,49 +16,48 @@
         // 1px solid #ced4da
         function validateName(nameString) {
             if (!nameString) {
-                    updateDOMElementInnerHTML("name", "You must provide an event name");
-                    updateDOMElementColour("name", "1px solid red")
+                    updateErrorStyling("name", "You must provide an event name", "1px solid red");
                     return false;
             } else if (dates[nameString]){
-                updateDOMElementInnerHTML("name", "A timer with that name already exists.");
-                updateDOMElementColour("name", "1px solid red")
+
+                updateErrorStyling("name", "A timer with that name already exists.", "1px solid red");
+
                     return false;
                 }
-                    console.log('trig in else')
-                    updateDOMElementInnerHTML("name", "");
-                    updateDOMElementColour("name", "1px solid #ced4da")
-
+                updateErrorStyling("name", "", "1px solid #ced4da");
                     return true;
         }
 
         function validateDate(timestamp) {
             if (!timestamp) {
-                    updateDOMElementInnerHTML("date", "You must provide an event date");
-                    updateDOMElementColour("date", "1px solid red");
+                updateErrorStyling("date", "You must provide an event date", "1px solid red");
+
 
                     return false;
                 } else {
                     if ((timestamp - Date.now()) < 0) {
-                        updateDOMElementInnerHTML("date", "You cannot enter a date in the past");
-                    updateDOMElementColour("date", "1px solid red");
+
+                updateErrorStyling("date", "You cannot enter a date in the past", "1px solid red");
+
 
                         return false;
                         
                     } else if (timestamp > (Date.now() + 8640000000)) {
-                        updateDOMElementInnerHTML("date", "You cannot enter a date that far in the future");
-                    updateDOMElementColour("date", "1px solid red");
+                updateErrorStyling("date", "You cannot enter a date that far in the future", "1px solid red");
+
 
                         return false;
                         
                     } else if (Object.values(dates).includes(timestamp)){
-                        updateDOMElementInnerHTML("date", "A countdown to that date already exists");
-                    updateDOMElementColour("date", "1px solid red");
+
+                updateErrorStyling("date", "A countdown to that date already exists", "1px solid red");
+
 
                         return false;
                         
                     } else {
-                        updateDOMElementInnerHTML("date", "");
-                    updateDOMElementColour("date", "1px solid #ced4da");
+                updateErrorStyling("date", "", "1px solid #ced4da");
+
 
                         return true;
                     }
@@ -82,23 +78,24 @@
 
         // }
 
-        function validateInputs() {
+        function validateInputs(eventName, eventDate) {
+            if (validateName(eventName) && validateDate(eventDate)) {
+                return true;
+            }  
+            return false;
+        }
+
+        function startTimer(){
             var eventName = document.getElementById("event-name").value;
             var eventTime = document.getElementById("event-time").value || "00:00";
             var eventDate = Date.parse(document.getElementById("event-date").value + 'T' + eventTime);
-            var nameValid = validateName(eventName);
-            var dateValid = validateDate(eventDate);
-            
-            console.log(nameValid, ' - name, ', dateValid, ' -date')
 
-            if (nameValid && dateValid) {
-                console.log('valid');
-                // check if name and timestamp already exists on dates object
-                // dates.push({name: eventName, date: eventDate});
+            if (validateInputs(eventName, eventDate)) {
                 dates[eventName] = eventDate;
-                createTimers(eventName, eventDate);
-            }  
-        }
+                workOutTimeLeft(eventName, eventDate);
+                createTimers();
+            }
+        } 
 
 
         function createTimers() {
@@ -109,22 +106,50 @@
             for (let [key, value] of Object.entries(dates)) {
                 timers.push({name: key, timer: setInterval(() => workOutTimeLeft(key, value), 1000) })
             }
-            // timers = dates.map(date => {
-            //     console.log(date);
-            //     return { name : date.name, timer : setInterval(() => workOutTimeLeft(date.name, date.date), 1000) };
-            // })
-            // workOutTimeLeft(name, date);
-            // foo = setInterval(() => workOutTimeLeft(name, date), 1000)
         }
-        function removeDOMElement(name) {
-            var element = document.getElementById(`${name}-countdown-timer`);
-            element.parentNode.removeChild(element);
+        function removeDOMElement(id, name) {
+            const countdownList = document.getElementById('countdown-list');
+
+            console.log(countdownList, countdownList.children);
+
+
+            if (name) {
+                delete dates[name];
+                clearInterval(timers.find(timer => timer.name === name).timer)
+                timers = timers.filter(timer => timer.name != name);
+            }
+            
+            var element = document.getElementById(id);
+            countdownList.removeChild(element);
+
+            console.log(countdownList, countdownList.children);
+
+            // if (!countdownList.children.length) {
+            //     console.log('inside')
+            //     let countdownLi = document.createElement("li");
+            //     countdownLi.setAttribute("class", "list-group-item");
+            //     countdownLi.setAttribute("id", "no-countdowns-li");
+            //     countdownLi.innerHTML('No countdowns set')
+            //     countdownList.appendChild(countdownLi);
+
+
+
+            // }
         }
 
         function workOutTimeLeft(name, date) {
             let now = Date.now()
-            let countdownString = '';
-            let years, months, weeks, days, hours, minutes, seconds;
+
+            console.log(now, date)
+            if (now > date) {
+                // handle a timer that has been reached
+                // delete timer and have a pop-up, or change the timer somehow
+                clearInterval(timers.find(timer => timer.name === name).timer)
+                $("#exampleModal").modal();
+                var audio = document.getElementById("alert")
+                audio.play();
+            }
+            let days, hours, minutes, seconds;
             let milisecondsUntil = date - now;
 
             days = String(Math.floor(milisecondsUntil / (1000 * 60 * 60 * 24))).padStart(2, "0");
@@ -132,22 +157,27 @@
             minutes = String(Math.floor((milisecondsUntil % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
             seconds = String(Math.floor((milisecondsUntil % (1000 * 60)) / 1000)).padStart(2, "0");
 
-            console.log(String(days).padStart(2, "0"), hours, minutes, seconds);
-
-            // console.log(countdownString);
-
         //     <ul id="countdown-list" class="list-group list-group-flush">
         //     <li class="list-group-item">No countdowns set</li>
         //   </ul>
+            let emptyListItem = document.getElementById('no-countdowns-li');
 
-            if(!document.getElementById(`${name}-countdown-timer`)) {
+            if(emptyListItem) {
+                removeDOMElement('no-countdowns-li')
+            }
+
+            if(!document.getElementById(`${name}-countdown-timer-div`)) {
                 let countdownLi = document.createElement("li");
-                let deleteButton = document.createElement("BUTTON");
-                deleteButton.onclick = () => removeDOMElement(name)
-                let countdownDiv = document.createElement("div");
-                deleteButton.innerHTML = '<i class="far fa-window-close"></i>';
-                countdownDiv.setAttribute("id", `${name}-countdown-timer`)
+                countdownLi.setAttribute("id", `${name}-countdown-timer`);
                 countdownLi.setAttribute("class", "list-group-item");
+
+                let deleteButton = document.createElement("BUTTON");
+                deleteButton.onclick = () => removeDOMElement(`${name}-countdown-timer`, name)
+                
+                let countdownDiv = document.createElement("div");
+                countdownDiv.setAttribute("id", `${name}-countdown-timer-div`);
+
+                deleteButton.innerHTML = '<i class="far fa-window-close"></i>';
 
                 countdownDiv.innerHTML = `${name}: ${days}d ${hours}h ${minutes}m ${seconds}s`
                 // console.log(document.getElementById(`${name}-countdown-timer`));
@@ -156,7 +186,7 @@
                 document.getElementById("countdown-list").appendChild(countdownLi);
 
             } else {
-                document.getElementById(`${name}-countdown-timer`).innerHTML = `${name}: ${days}d ${hours}h ${minutes}m ${seconds}s`
+                document.getElementById(`${name}-countdown-timer-div`).innerHTML = `${name}: ${days}d ${hours}h ${minutes}m ${seconds}s`
             }
 
             // console.log(years, months, weeks, days, hours, minutes, seconds);
